5c0014c346358c7c9824f1cf75466a5d9cc2843b
atoms <you@example.com>
2019-01-25 17:43:28 -0200

    testing eclipse scientific computing release

---
+++ eclipse_test2/src/test2.f90
@@ -0,0 +1,22 @@
+! ============================================================================
+! Name        : test2.f90
+! Author      : mee
+! Version     :
+! Copyright   : Your copyright notice
+! Description : Hello World in Fortran
+! ============================================================================
+
+program test2
+    implicit none
+    print *, "Hello  World"
+    print *, "Hello  World"
+    print *, "Hello  World"
+    print *, "Hello  World"
+    print *, "Hello  World"
+    print *, "Hello  World"
+    print *, "Hello  World"
+    print *, "Hello  World"
+    print *, "Hello  World"
+    print *, "Hello  World"
+    print *, "Hello  World"
+end program
